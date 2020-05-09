dcc07addb406cd3c0f27f3286b729e2b36e97bdb
asapcoding <65024635+asapcoding@users.noreply.github.com>
2020-05-09 13:01:34 +0200

    Update saveexcelfile.py

--- saveexcelfile.py
+++ saveexcelfile.py
@@ -3,37 +3,46 @@ Created on 07.05.2020
 
 @author: asapcoding
 '''
-'''libraries import'''
+'''libraries import / Bibliotheken einbinden '''
 from tkinter import filedialog
 from openpyxl import Workbook
 
-'''functions'''
+'''function / Funktion '''
 def savefile(newvalues):
     # ask user which save path
+    # fragt den Benutzer wo das Ergebnis gespeichert werden soll
     file_path = filedialog.asksaveasfilename(title = "Exceldatei speichern",filetypes = (("excel files","*.xlsx"),))
     
-    # if user choose path, else if not 
+    # if: user choose path, else: if not 
+    # if: Benutzer hat Pfad und Name ausgewählt, else: wenn nicht
     if file_path:
         
-        # get workbook and active it
+        # gets workbook and active it
+        # holt Arbeitsmappe und aktiviert sie
         book = Workbook()
         ws = book.active
         
-        # convert integers to string
+        # converts integers to string
+        # konvertiert Ganzzahlen in Zeichenfolgen
         valu = newvalues.astype('S')
         
-        # convert matrix to list
+        # converts matrix to list
+        # konvertiert Matrix zu Liste
         val = valu.tolist()
         
         # append list to workbook
+        # Liste an Arbeitsmappe anhängen
         for row in val:
             ws.append(row)
         
         # saves workbook to the chosen path 
+        # speichert die Exceldatei im ausgewählten Pfad
         book.save(file_path)
         
         # returns 1 if the file was saved
+        # Gibt 1 zurück, wenn die Datei gespeichert wurde
         return 1
     else:
         # returns 0 if the file was not saved
+        # gibt 0 zurück, wenn die Datei nicht gespeichert wurde
         return 0
